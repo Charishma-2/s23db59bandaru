8ebdc671004418a8700e4c8a38abaa362f228b56
Charishma-2 <charishmachowdarybandaru@gmail.com>
2023-04-24 20:02:07 -0500

    chnages made by me

--- models/tennis.js
+++ models/tennis.js
@@ -1,7 +1,14 @@
 const mongoose = require("mongoose")
 const tennisSchema = mongoose.Schema({
-Player_Name: String,
-Player_Age : String,
-No_Of_Matches_Played: Number
+    Player_Name: {
+        type: String,
+        required: [true, "Name required"]
+    },
+    Player_Age: Number,
+    No_Of_Matches_Played: {
+        type: Number,
+        min: [5, "Min value is 5"],
+        max: [50, "Max value is 50"]
+    }
 })
 module.exports = mongoose.model("tennis",tennisSchema)
--- routes/tennis.js
+++ routes/tennis.js
@@ -7,9 +7,6 @@ module.exports = router;
 
 
 router.get('/detail', tennis_controlers.tennis_view_one_Page);
-router.get('/create', tennis_controlers.tennis_create_Page);
-router.get('/update', tennis_controlers.tennis_update_Page);
-router.get('/delete', tennis_controlers.tennis_delete_Page);
 
 //var express = require('express');
 //var router = express.Router();
@@ -21,3 +18,17 @@ router.get('/delete', tennis_controlers.tennis_delete_Page);
 //});
 
 //module.exports = router;
+
+// A little function to check if we have an authorized user and continue on
+//or
+// redirect to login.
+const secured = (req, res, next) => {
+ if (req.user){
+ return next();
+ }
+ req.session.returnTo = req.originalUrl;
+ res.redirect("/login");
+ }
+router.get('/create',secured, tennis_controlers.tennis_create_Page);
+router.get('/update', secured,tennis_controlers.tennis_update_Page);
+router.get('/delete', secured,tennis_controlers.tennis_delete_Page);
